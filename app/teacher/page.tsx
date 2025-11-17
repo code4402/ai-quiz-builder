eafb6aa481b4a94e34ae4fb1516024f7b2d91336
code4402 <dumbledorehow@gmail.com>
2025-11-17 22:31:48 +0530

    updated nav and teacherlogin

--- app/teacher/page.tsx
+++ app/teacher/page.tsx
@@ -1,5 +1,6 @@
 "use client";
-import ProtectedRoute from "@/components/ProtectedRoute";
+ import ProtectedRoute from "../components/ProtectedRoute";
+;
 
 export default function TeacherDashboard() {
   return (
