0274a0589a839415b2d1ab6d7ac1bd4122b6bada
code4402 <dumbledorehow@gmail.com>
2025-11-22 20:00:14 +0530

    cd

---
+++ app/unauthorized/page.tsx
@@ -0,0 +1,7 @@
+export default function Unauthorized() {
+  return (
+    <div className="p-10 text-center text-red-600 text-xl">
+      ❌ You are not allowed to access this page.
+    </div>
+  );
+}
